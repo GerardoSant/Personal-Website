7ad6960aa27d6ddddca0bcd64566c4fb48b6b6f9
GerardoSant <gbuenclima@gmail.com>
2020-08-26 20:27:01 +0200

    Added option to see the CV from the webpage

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,15 @@
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,6 +26,29 @@ public class main {
                     new ModelAndView(model, "/velocity/index.html")
             );
         });
+        get("/cv", (req, res) -> {
+            HttpServletResponse httpServletResponse = res.raw();
+            httpServletResponse.setContentType("application/pdf");
+            httpServletResponse.addHeader("Content-Disposition", "inline; filename=CV.pdf");
+            httpServletResponse.getOutputStream().write(getCV());
+            httpServletResponse.getOutputStream().close();
+            return "";
+        });
+    }
+
+    private static byte[] getCV() throws IOException {
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet("https://github.com/GerardoSant/GerardoSant/raw/master/cv/CV.pdf");
+        HttpResponse response = client.execute(request);
+        HttpEntity entity = response.getEntity();
+        InputStream inputStream = entity.getContent();
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int nRead;
+        byte[] data = new byte[16384];
+        while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+        return buffer.toByteArray();
     }
 
     static int getHerokuAssignedPort() {
